4e01442ad379dc7183343242cc07e55c280fe199
J Smith <dark.panda@gmail.com>
2017-02-21 15:05:37 -0400

    Fix PostGIS 2.2+ deprecation notices.

--- lib/activerecord-spatial/spatial_scope_constants.rb
+++ lib/activerecord-spatial/spatial_scope_constants.rb
@@ -33,7 +33,7 @@ module ActiveRecordSpatial
 
     ONE_GEOMETRY_ARGUMENT_MEASUREMENTS = %w{
       distance
-      distance_sphere
+      distancesphere
       maxdistance
     }
 
@@ -42,73 +42,24 @@ module ActiveRecordSpatial
       dfullywithin
     }
 
-    ONE_ARGUMENT_MEASUREMENTS = %w{
-      length2d_spheroid
-      length_spheroid
-    }
-
-    # Some functions were renamed in PostGIS 2.0.
-    if ActiveRecordSpatial::POSTGIS[:lib] >= '2.0'
-      RELATIONSHIPS.concat(%w{
-        3dintersects
-      })
-
-      ZERO_ARGUMENT_MEASUREMENTS.concat(%w{
-        3dlength
-        3dperimeter
-      })
-
-      ONE_ARGUMENT_MEASUREMENTS.concat(%w{
-        3dlength_spheroid
-      })
-
-      ONE_GEOMETRY_ARGUMENT_MEASUREMENTS.concat(%w{
-        3ddistance
-        3dmaxdistance
-      })
-
-      ONE_GEOMETRY_ARGUMENT_AND_ONE_ARGUMENT_RELATIONSHIPS.concat(%w{
-        3ddwithin
-        3ddfullywithin
-      })
-    else
-      ZERO_ARGUMENT_MEASUREMENTS.concat(%w{
-        length3d
-        perimeter3d
-      })
-
-      ONE_ARGUMENT_MEASUREMENTS.concat(%w{
-        length3d_spheroid
-      })
-    end
-
     FUNCTION_ALIASES = {
       'order_by_st_max_distance' => 'order_by_st_maxdistance',
       'st_geometrytype' => 'st_geometry_type'
     }
 
-    COMPATIBILITY_FUNCTION_ALIASES = if ActiveRecordSpatial::POSTGIS[:lib] >= '2.0'
-      {
-        'order_by_st_length3d' => 'order_by_st_3dlength',
-        'order_by_st_perimeter3d' => 'order_by_st_3dperimeter',
-        'order_by_st_length3d_spheroid' => 'order_by_st_3dlength_spheroid'
-      }
+    COMPATIBILITY_FUNCTION_ALIASES = {}
+
+    # Some functions were renamed or deprecated in PostGIS 2.2.
+    if ActiveRecordSpatial::POSTGIS[:lib] >= '2.2'
+      require 'activerecord-spatial/spatial_scope_constants/postgis_2_2'
+    elsif ActiveRecordSpatial::POSTGIS[:lib] >= '2.0'
+      require 'activerecord-spatial/spatial_scope_constants/postgis_2_0'
     else
-      {
-        'order_by_st_3dlength' => 'order_by_st_length3d',
-        'order_by_st_3dperimeter' => 'order_by_st_perimeter3d',
-        'order_by_st_3dlength_spheroid' => 'order_by_st_length3d_spheroid'
-      }
+      require 'activerecord-spatial/spatial_scope_constants/postgis_legacy'
     end
 
-    if ActiveRecordSpatial::POSTGIS[:lib] >= '2.0'
-      FUNCTION_ALIASES.merge!({
-        'st_3d_dwithin' => 'st_3ddwithin',
-        'st_3d_dfully_within' => 'st_3ddfullywithin',
-        'order_by_st_3d_distance' => 'order_by_st_3ddistance',
-        'order_by_st_3d_max_distance' => 'order_by_st_3dmaxdistance'
-      })
+    self.constants.each do |constant|
+      constant.freeze
     end
   end
 end
-
